8c48203ffce0d5de8085f354e3dcd2f4e09a4d1e
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-18 17:02:02 +0530

    Validating Email

--- Program.cs
+++ Program.cs
@@ -1,24 +1,14 @@
 ﻿namespace UserRegistrationProblem
 {
-    class Program
+    public class Program
     {
         public static void Main(String[] args)
         {
-            RegexClass regex = new RegexClass();
-            regex.ValidateFirstNameRegex("Bhagyashree");
-            regex.ValidateLastNameRegex("Nemade");
-            string[] EmailList = { "abc@yohoo.com","abc-100@yohoo.com",
-                                    "abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net",
-                                     "abc.100@abc.com.au", "abc@1.com","abc","abc@.com.my",
-                                    "abc123@gmail.a",
-                                     "abc1232.com"
-                                     ,".abc@abc.com","abc123.com.my", "abc123@.com.com","bhagyashreenemade2000@gmail.com"};
+            Console.WriteLine("Welcome to user registration problem");
 
-            regex.ValidateEmailAddressRegex("b.nemade@rgit.com");
-            foreach (string SampleEmailAddress in EmailList)
-            {
-                regex.ValidateEmailAddressRegex(SampleEmailAddress);
-            }
+            RegexClass user = new RegexClass();
+            user.regexClass();
+
         }
     }
 }
