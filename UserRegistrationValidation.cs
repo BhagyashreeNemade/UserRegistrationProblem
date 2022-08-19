d2f0dce6f0ad4b3e87229c40aebb021db1134e09
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-19 16:51:42 +0530

    Email Validation

---
+++ UserRegistrationValidation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class UserRegistrationValidation
+    {
+      
+        private static string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
+        private static string REGEX_LASTNAME = "^[A-Z][a-z]{2,}$";
+        private static string REGEX_EMAIL = "^[a-zA-Z0-9]+([.][A-Za-z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]+)?$";
+       
+        //Method to test first name
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }
+
+        //Method to test last name
+        public bool ValidateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, REGEX_LASTNAME);
+        }
+
+        //Method to test Email ID
+        public bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+
+        //To print the result
+        public void PrintResult(bool result)
+        {
+            if (result)
+            {
+                Console.WriteLine("Valid.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid.");
+            }
+        }
+
+     
+    }
+}
