596c7b62eeac3e3609b519a4616add6e43208623
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-19 16:44:02 +0530

    Test cases for User Registration problem

---
+++ UnitTest1.cs
@@ -0,0 +1,123 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UserRegistrationProblem;
+
+namespace ValidationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Happy Test Cases(Test Cases Pass The Entries)
+        [TestMethod]
+        public void GivenUserFistName_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string firstName = "Bhagyashree";
+            //Act
+            bool result = user.ValidateFirstName(firstName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenUserLastName_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string lastName = "Nemade";
+            //Act
+            bool result = user.ValidateFirstName(lastName);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenEmailId_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string email = "bhagunemade7@gmail.com";
+            //Act
+            bool result = user.ValidateEmail(email);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenMobileNumber_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string mobileNumber = "91 7999837990";
+            //Act
+            bool result = user.ValidateMobileNumber(mobileNumber);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void GivenPassword_WhenValidate_ShouldReturnTrue()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string password = "Bhagu@123";
+            //Act
+            bool result = user.ValidatePassword(password);
+            //Assert
+            Assert.IsTrue(result);
+        }
+        //Sad Test Cases(Test Cases Fail The Entry)
+        [TestMethod]
+        public void GivenUserFistName_WhenValidate_ShouldReturnFalse()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string firstName = "bhagu";
+            //Act
+            bool result = user.ValidateFirstName(firstName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenUserLastName_WhenValidate_ShouldReturnFalse()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string lastName = "nemade";
+            //Act
+            bool result = user.ValidateFirstName(lastName);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenEmailId_WhenValidate_ShouldReturnFalse()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string email = "bhagungmail.com";
+            //Act
+            bool result = user.ValidateEmail(email);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenMobileNumber_WhenValidate_ShouldReturnFalse()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string mobileNumber = "91 95876006";
+            //Act
+            bool result = user.ValidateMobileNumber(mobileNumber);
+            //Assert
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void GivenPassword_WhenValidate_ShouldReturnFalse()
+        {
+            //Arrange
+            UserRegistrationValidation user = new UserRegistrationValidation();
+            string password = "bhagu@#123";
+            //Act
+            bool result = user.ValidatePassword(password);
+            //Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
